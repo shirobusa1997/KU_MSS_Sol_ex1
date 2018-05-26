45cee854383ad31de7d97f3d78db6d10c051d006
MOJATTO STUDIO <daichiandroid1997@gmail.com>
2018-05-26 23:27:02 +0900

    Add Function

--- source.cpp
+++ source.cpp
@@ -1,12 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-void ud_input(FILE *fp, int ud[40][12]){
+#define DEBUG
 
+#define ARRAYSIZE_ROW 40
+#define ARRAYSIZE_COLLUM 12
+
+#define MAX_BUFFERSIZE 2048
+
+#pragma warning(disable:4996)
+
+enum direction{North, East, South, West};
+
+struct Transform{
+    int Pos_x;
+    int Pos_y;
+    enum direction current_direction;
+};
+
+//入力されたCSVデータの内容を配列変数に置き換える関数"ud_input"です。
+void ud_input(FILE *fp, int ud[ARRAYSIZE_ROW][ARRAYSIZE_COLLUM]){
+    char buffer[MAX_BUFFERSIZE] = {0};
+    char *buffer2[ARRAYSIZE_COLLUM];
+    #ifdef DEBUG
+       printf("func:ud_input   :\nConvert from CSV to array data...\n");
+    #endif
+
+    for(int count1 = 0; count1 < ARRAYSIZE_ROW; count1++){
+        fgets(buffer, MAX_BUFFERSIZE, fp);
+        for(int count2 = 0; count2 < ARRAYSIZE_COLLUM; count2++){
+            if(count2 == 0)
+            {
+                buffer2[count2] = strtok(buffer,",");
+            }else{
+                buffer2[count2] = strtok(NULL,",");
+            }
+        }
+        for(int count2 = 0; count2 < ARRAYSIZE_COLLUM; count2++){
+            ud[count1][count2] = atoi(buffer2[count2]);
+
+            #ifdef DEBUG
+               printf("%d ",ud[count1][count2]);
+            #endif
+        }
+
+        #ifdef DEBUG
+           printf("\n");
+        #endif
+    }
+
+    #ifdef DEBUG
+       printf("func:ud_input   :\n--FUNCTION PROCESS COMPLETE--\n");
+    #endif
 }
 
 int main(int argc, char **argv){
     //変数を宣言します。
-    int ud[40][12] = {0};
+    int ud[ARRAYSIZE_ROW][ARRAYSIZE_COLLUM] = {0};
     FILE *fp;
 
     if(argc < 2){
@@ -23,7 +74,10 @@ int main(int argc, char **argv){
     ud_input(fp, ud);
 
     fclose(fp);
-    printf("\n\n--SOLUTION COMPLETED--\n");
+
+    #ifdef DEBUG
+       printf("\n\n--SOLUTION COMPLETE--\n");
+    #endif
     
     return 0;
 }
